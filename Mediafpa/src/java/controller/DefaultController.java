7ad7fc4d42c7617b22cf791afc222d06185eda39
Unknown <beauvoir.nicolas@gmail.com>
2017-08-03 14:05:47 +0200

    Database - DAO

--- Mediafpa/src/java/controller/DefaultController.java
+++ Mediafpa/src/java/controller/DefaultController.java
@@ -5,14 +5,28 @@
  */
 package controller;
 
-/**
- *
- * @author Moi
- */
-public class DefaultController {
+import access.DaoDocument;
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import model.Document;
+
+@Named
+@ViewScoped
+public class DefaultController implements Serializable {
+	
+	@Inject
+	DaoDocument daoDocument;
 	
 	public DefaultController() {
 		
+	}
+	
+	public void DaoTest() {
+		Document document = daoDocument.findByKey(1);
+		
+		System.out.println(document.getTitre());
 	}
 	
 }
